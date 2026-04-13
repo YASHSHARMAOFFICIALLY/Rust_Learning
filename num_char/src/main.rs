77c7dd5d346e0b901f6aab8fe2da90fa0f608d9d
Yash sharma <yashsharmaofficially@gmail.com>
2026-04-14 01:09:02 +0530

    added num_char

--- num_char/src/main.rs
+++ num_char/src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+fn check_num(num: i32) -> String {
+    if num == 0 {
+        return "Zero".to_string();
+    }
+
+    if num > 0 {
+        if num % 2 == 0 {
+            return "Positive even".to_string();
+        } else {
+            return "Positive odd".to_string();
+        }
+    } else {
+        if num % 2 == 0 {
+            return "Negative even".to_string();
+        } else {
+            return "Negative odd".to_string();
+        }
+    }
+}
+fn main(){
+    println!("Enter the num");
+
+    let mut num = String::new();
+        io::stdin()
+        .read_line(&mut num)
+        .expect("Failed to expect the line");
+
+    let num:i32 = num.trim().parse().expect("Failed to read");
+
+    let validate = check_num(num);
+    println!("{:?}",validate)
+
 }
